b2b342b1e42486af9941117011df0b4d7fa9ef01
byetiskin <begum.yetiskin@inonu.edu.tr>
2024-10-21 10:31:29 +0300

    Update uyg5.py

--- Hafta3-4/uyg5.py
+++ Hafta3-4/uyg5.py
@@ -1,24 +1,20 @@
 class Student:
     def __init__(self, name, grade):
-        self.__name = name  # Private değişken
-        self._grade = grade  # Protected değişken
+        self.__name = name
+        self.grade = grade  
 
-    # Getter for name
     @property
     def name(self):
         return self.__name
 
-    # Setter for name
     @name.setter
     def name(self, name):
         self.__name = name
 
-    # Getter for grade
     @property
     def grade(self):
         return self._grade
 
-    # Setter for grade
     @grade.setter
     def grade(self, grade):
         if 0 <= grade <= 100:  # Geçerli bir not aralığı
@@ -26,18 +22,9 @@ class Student:
         else:
             raise ValueError("Not 0 ile 100 arasında olmalı")
 
-# Kullanım
-student = Student("Ali", 85)
-print(student.name)  # Ali
-print(student.grade)  # 85
-
-student.name = "Ayşe"
-print(student.name)  # Ayşe
-student.grade = 90
-print(student.grade)  # 90
-
-# Geçersiz bir not atanıyor
 try:
-    student.grade = 120  # ValueError: Not 0 ile 100 arasında olmalı
+    student = Student("Ali", 140) 
+    print(student.name)
+    print(student.grade)
 except ValueError as e:
-    print(e)
+    print(e)  
