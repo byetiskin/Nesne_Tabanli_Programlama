69da5b8ba250473bf74980703bffa3c2e50ba16f
byetiskin <begum.yetiskin@inonu.edu.tr>
2024-11-11 02:44:19 +0300

    Add files via upload

---
+++ Hafta7-8/ekran_ciktilari2.py
@@ -0,0 +1,14 @@
+for i in range(3): 
+    print("*" * 5)  
+# %%
+for i in range(1, 6):  
+    print("*" * i)
+# %%
+for i in range(5): 
+    print(" " * i + "*" * (5 - i))  
+# %%
+for i in range(1, 6):  
+    print(" " * (5 - i) + "*" * (2 * i - 1))  
+# %%
+for i in range(5, 0, -1):
+    print(" " * (5 - i) + "*" * (2 * i - 1))  
